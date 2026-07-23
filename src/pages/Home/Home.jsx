546a7303dbe5f0e088d311e100d410893208a3cc
Lakshya Singh <sinlakshya17@gmail.com>
2026-07-23 09:31:11 +0530

    initial

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
   return (
     <div>
         <Header />
-        hdsbghfghdsghfsd
         <ExploreMenu category={category} setCategory={setCategory} />
         <FoodDisplay category={category}/>
     </div>
